eb6976730c549594799f0027a5b2459bbb784373
gitterman2022 <ananta.srabon.aranyak.asa@gmail.com>
2022-04-23 21:37:50 +0600

    almost done this home work

---
+++ index.js
@@ -0,0 +1,33 @@
+//get the button
+var mybutton = document.getElementById("myBtn");
+
+// when the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// when the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// selecting elements
+const dateSec = document.getElementById('date');
+
+// todays date
+const option = {
+    weekday : "long",
+    month : "short",
+    day : "numeric"
+};
+
+const today = new Date();
+
+dateSec.innerHTML = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) + ", " + today.toLocaleDateString("en-US", option);
