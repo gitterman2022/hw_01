703603f97c8bafcc1161eb4bb56f3b882b8986fb
gitterman2022 <ananta.srabon.aranyak.asa@gmail.com>
2022-05-07 23:05:24 +0600

    menu toggler added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// menu toggling
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".nav");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
 //get the button
 var mybutton = document.getElementById("myBtn");
 
